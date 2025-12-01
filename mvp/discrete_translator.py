d6587a08f5d4fd56b602f2346ce736d8aac150da
ryypow <191621125+ryypow@users.noreply.github.com>
2025-12-01 18:11:42 +0000

    updaqted actionbins - fixed logic for rotate positive action

--- mvp/discrete_translator.py
+++ mvp/discrete_translator.py
@@ -25,7 +25,7 @@ class ActionTranslator(gym.ActionWrapper):
 
         elif act == 2: #rotation POSITIVE (+Z direction)
             continuous[0:4] = self.strong_movement * 1.5   # stronger push to move cube more
-            continuous[4:8] = -self.strong_movement  #slightly releases
+            continuous[4:8] = -self.weak_movement  #slightly releases
             continuous[8:12] = -self.strong_movement  # finger3 OPENS (allows rotation)
             continuous[12:16] = self.weak_movement    # thumb stabilizes
 
